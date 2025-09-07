fed461c7905a359e117965f9b0dedd2e70229e77
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-07 16:09:25 +0200

    refactor: remove unnecessary imports

--- src/core/Text.js
+++ src/core/Text.js
@@ -1,3 +1,3 @@
-import { TTFLoader } from "three/addons/loaders/TTFLoader.js";
+import * as THREE from "three";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";

c546b6f0291e3b2e1f0e04b556b7b4cf64d5748c
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-07 17:38:07 +0200

    feat: add and export createTextObject() in TextLoader.js

--- src/loaders/TextLoader.js
+++ src/loaders/TextLoader.js
@@ -3,3 +3,18 @@ import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 
 const loader = new FontLoader();
+
+const createTextObject = (text, fontUrl) => {
+  return new Promise((resolve, reject) => {
+    loader.load(fontUrl, (font) => {
+      const textGeometry = new TextGeometry(text, {
+        font: font,
+      });
+      const textMaterial = new THREE.MeshBasicMaterial();
+      const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+      resolve(textMesh);
+    });
+  });
+};
+
+export { createTextObject };

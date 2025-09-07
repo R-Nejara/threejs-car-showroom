64c097d66c3ea904ef98f2c60c89e990bf66b010
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-07 03:54:36 +0200

    feat: add scene backgroundcolor and fog in Scene.js

--- src/core/Scene.js
+++ src/core/Scene.js
@@ -4,6 +4,11 @@ import { modelGroup } from "./Showcase.js";
 import { lights } from "./Light.js";
 // Initialize Scene
 const scene = new THREE.Scene();
+
+// Fog
+scene.fog = new THREE.Fog("#0088ff", 20, 50);
+scene.background = new THREE.Color("#0088ff");
+
 // Addings To Scene
 scene.add(camera);
 scene.add(modelGroup);

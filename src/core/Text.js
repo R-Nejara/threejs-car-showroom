c3540b084aae23d84fdc7c8932550f0851f5881e
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-07 15:28:40 +0200

    chore: prepare imports in Text.js

---
+++ src/core/Text.js
@@ -0,0 +1,3 @@
+import { TTFLoader } from "three/addons/loaders/TTFLoader.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";

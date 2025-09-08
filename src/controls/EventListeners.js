9f6db829e75bec22834532dd6e1383fa346025c3
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 21:58:44 +0200

    feat add eventlistener for start button in EventListeners.js

--- src/controls/EventListeners.js
+++ src/controls/EventListeners.js
@@ -1,6 +1,18 @@
+import { animateStart, textFadeOutAnimation } from "../animations/Animations";
 import { updateCamera } from "../core/Camera";
 import { updateRenderer } from "../core/Renderer";
+import { startButton, startText } from "../Selectors";
+import { toggleControls } from "./OrbitControls";
+import { titleText, nameText } from "../core/Text";
 window.addEventListener("resize", () => {
   updateCamera();
   updateRenderer();
+});
+
+startButton.addEventListener("click", function () {
+  startButton.classList.add("hidden");
+  textFadeOutAnimation(titleText);
+  textFadeOutAnimation(nameText);
+  toggleControls();
+  animateStart();
 });

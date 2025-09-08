48898d0e48e496d4b3f6f282c84fbc7d2f5381e8
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 18:34:22 +0200

    fix: correct controls.target in OrbitControls.js

--- src/controls/OrbitControls.js
+++ src/controls/OrbitControls.js
@@ -2,11 +2,10 @@ import * as THREE from "three";
 import { camera } from "../core/Camera.js";
 import { canvas } from "../Selectors.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { carPosition } from "../core/Showcase.js";
 
 const controls = new OrbitControls(camera, canvas);
-controls.target.copy(carPosition);
-console.log(controls.getPolarAngle(controls));
+controls.target.copy(skyPosition);
+
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 

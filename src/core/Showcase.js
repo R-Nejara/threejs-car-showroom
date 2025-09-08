98fc7384d61e0d0b6a12f349a99ce3d257b821f4
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 18:31:39 +0200

    chore: move unfitting variables to helpers.js

--- src/core/Showcase.js
+++ src/core/Showcase.js
@@ -7,8 +7,20 @@ modelGroup.traverse((child) => {
   child.receiveShadow = true;
 });
 
-// Car and Sky Position as Vector3
+// Car And Sky Position As Vector3
 const carPosition = new THREE.Vector3(-5.5, 2, -3);
 const skyPosition = new THREE.Vector3(-5.5, 20, -3);
 
-export { modelGroup, carPosition, skyPosition };
+// Usefull Constants
+const GROUND_LEVEL_SIDEWALK_Y = 0.6;
+const GROUND_LEVEL_STREET_Y = 0.4;
+const SKY_LEVEL_Y = skyPosition.y;
+
+export {
+  modelGroup,
+  carPosition,
+  skyPosition,
+  GROUND_LEVEL_SIDEWALK_Y,
+  GROUND_LEVEL_STREET_Y,
+  SKY_LEVEL_Y,
+};

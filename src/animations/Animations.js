5909429ee624292ce1f5c04825518fd4583ba780
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 16:09:57 +0200

    feat: add start animation for camera and target

--- src/animations/Animations.js
+++ src/animations/Animations.js
@@ -1,3 +1,28 @@
 import * as THREE from "three";
 import { camera } from "../core/Camera";
 import { controls } from "../controls/OrbitControls";
+import { gsap } from "gsap";
+import { carPosition, skyPosition } from "../core/Showcase";
+
+// Helper Variables
+let isStarted = false;
+
+// Start Animation_START:
+const cameraPositionAnimation = () => {
+  gsap.to(camera.position, {
+    y: carPosition.y,
+    duration: 5,
+    ease: "circ.inOut",
+  });
+};
+
+const targetPositionAnimation = () => {
+  gsap.to(controls.target, {
+    x: carPosition.x,
+    y: carPosition.y,
+    z: carPosition.z,
+    duration: 5,
+    ease: "circ.inOut",
+  });
+};
+// Start Animation_END:

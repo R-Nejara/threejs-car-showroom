e5759abe4fe828a0547b780d6f9f60e727b3853f
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 16:06:56 +0200

    fix: correct controls target in OrbitControls.js

--- src/controls/OrbitControls.js
+++ src/controls/OrbitControls.js
@@ -2,8 +2,10 @@ import * as THREE from "three";
 import { camera } from "../core/Camera.js";
 import { canvas } from "../Selectors.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { carPosition } from "../core/Showcase.js";
 
 const controls = new OrbitControls(camera, canvas);
+controls.target.copy(carPosition);
 
 const updateControls = () => {
   controls.update();

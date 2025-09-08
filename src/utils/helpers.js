68fc0fb9037b0c50a78e64b09979c0ce626a18aa
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 18:30:21 +0200

    chore: add helper constants in helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,15 @@
+import * as THREE from "three";
+
+// Helper Constants
+// Car And Sky Position
+const carPosition = new THREE.Vector3(-5.5, 2, -3);
+const skyPosition = new THREE.Vector3(-5.5, 20, -3);
+
+// Ground and Sky Y Constants
+const GROUND_LEVEL_SIDEWALK_Y = 0.6;
+const GROUND_LEVEL_STREET_Y = 0.4;
+const SKY_LEVEL_Y = skyPosition.y;
+
 // Initialize Sizes
 const sizes = {
   width: window.innerWidth,
@@ -13,4 +25,12 @@ const updateSizes = () => {
   sizes.height = window.innerHeight;
 };
 
-export { sizes, updateSizes }
+export {
+  sizes,
+  updateSizes,
+  carPosition,
+  skyPosition,
+  GROUND_LEVEL_SIDEWALK_Y,
+  GROUND_LEVEL_STREET_Y,
+  SKY_LEVEL_Y,
+};

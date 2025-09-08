6f81881c67518df7728c7935a738d3666e58cfb5
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 21:08:08 +0200

    chore: add query selectors of button and buttontext from index.html into Selectors.js

--- src/Selectors.js
+++ src/Selectors.js
@@ -1,4 +1,6 @@
 // Canvas
 const canvas = document.getElementById("main-canvas");
+const startButton = document.getElementById("start.button");
+const startText = document.getElementById("start-text");
 
 export { canvas };

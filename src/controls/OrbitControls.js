6d9291f56be407a4ce97352a106eb9523c9fe626
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 23:45:51 +0200

    feat: update comments, add limit for controls distance and disable controls pan in OrbitControls.js

--- src/controls/OrbitControls.js
+++ src/controls/OrbitControls.js
@@ -8,13 +8,18 @@ const controls = new OrbitControls(camera, canvas);
 controls.target.copy(skyPosition);
 
 controls.enableDamping = true;
+controls.enablePan = false;
 controls.dampingFactor = 0.05;
 
 // Disables Free movement
 controls.enabled = false;
+
 // Disables Unlimited Rotation
 controls.minPolarAngle = 0.55;
 controls.maxPolarAngle = Math.PI / 2;
+// Limit Distance
+controls.minDistance = 2;
+controls.maxDistance = 30;
 
 const updateControls = () => {
   controls.update();

ffc99ae51ea383521787fa9d81f92f17ce467153
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 18:22:41 +0200

    feat: add font paths to fonts.js

---
+++ src/utils/fonts.js
@@ -0,0 +1,23 @@
+const fonts = {
+  DROID_SANS_BOLD:
+    "/node_modules/three/examples/fonts/droid/droid_sans_bold.typeface.json",
+  DROID_SANS_REGULAR:
+    "/node_modules/three/examples/fonts/droid/droid_sans_regular.typeface.json",
+  DROID_SERIF_BOLD:
+    "/node_modules/three/examples/fonts/droid/droid_serif_bold.typeface.json",
+  DROID_SERIF_REGULAR:
+    "/node_modules/three/examples/fonts/droid/droid_serif_regular.typeface.json",
+  GENTILIS_BOLD:
+    "/node_modules/three/examples/fonts/gentilis_bold.typeface.json",
+  GENTILIS_REGULAR:
+    "/node_modules/three/examples/fonts/gentilis_regular.typeface.json",
+  HELVETIKER_BOLD:
+    "/node_modules/three/examples/fonts/helvetiker_bold.typeface.json",
+  HELVETIKER_REGULAR:
+    "/node_modules/three/examples/fonts/helvetiker_regular.typeface.json",
+  OPTIMER_BOLD: "/node_modules/three/examples/fonts/optimer_bold.typeface.json",
+  OPTIMER_REGULAR:
+    "/node_modules/three/examples/fonts/optimer_regular.typeface.json",
+};
+
+export { fonts };

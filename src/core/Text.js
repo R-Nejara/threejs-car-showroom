761988d4a46152573124dd91a507ba36cac39fef
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-08 18:52:00 +0200

    feat: initialize Texts and set initial positions/rotations in Text.js

--- src/core/Text.js
+++ src/core/Text.js
@@ -1,2 +1,22 @@
 import * as THREE from "three";
 import { createTextObject } from "../loaders/textLoader";
+import { fonts } from "../utils/fonts";
+import { SKY_LEVEL_Y } from "../utils/helpers";
+
+// Initialize Texts
+const titleText = await createTextObject("Car Project", fonts.DROID_SANS_BOLD);
+const nameText = await createTextObject(
+  "BY \nRAYAN NEJARA",
+  fonts.DROID_SANS_BOLD
+);
+
+// Set Initial Text Positions
+titleText.position.copy(new THREE.Vector3(-5, SKY_LEVEL_Y + 2.5, -15));
+nameText.position.copy(new THREE.Vector3(-5, SKY_LEVEL_Y + 1, -15));
+
+// Set Initial Text Rotations
+titleText.rotation.y = -0.5;
+nameText.rotation.y = -0.5;
+
+// Exports
+export { titleText, nameText };

89d5a03599c68485e37c2acd94618bd40605570f
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-10 18:22:38 +0200

    fix: correct position for webObject in WebObject.js

--- src/core/WebObject.js
+++ src/core/WebObject.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer.js";
 import { pannelSizes } from "./UserPannel";
+import { pannelPosition } from "../utils/helpers";
 
 // Create Iframe Object
 const iframe = document.createElement("iframe");
@@ -12,6 +13,9 @@ iframe.style.height = pannelSizes.height * 100 + "px";
 
 // Transfer HTML Element Into CSS3DObject
 const webObject = new CSS3DObject(iframe);
+
+// Set webObject to Pannel Position
+webObject.position.copy(pannelPosition);
 
 // Lower Scale For More PPI
 webObject.scale.setScalar(0.01);

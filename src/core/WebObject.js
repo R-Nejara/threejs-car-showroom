541e41783d08de973246f5f1da0cd3f6268d8bb8
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-11 08:49:11 +0200

    fix: see through iframe fixed in WebObject.js

--- src/core/WebObject.js
+++ src/core/WebObject.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer.js";
 import { pannelSizes } from "./UserPannel";
 import { pannelPosition } from "../utils/helpers";
+import { camera } from "./Camera.js";
 
 // Create Iframe Object
 const iframe = document.createElement("iframe");
@@ -13,6 +14,13 @@ iframe.style.border = "0px";
 
 // Transfer HTML Element Into CSS3DObject
 const webObject = new CSS3DObject(iframe);
+
+// Fix see through iframe
+const normal = new THREE.Vector3(0, 0, 1).applyQuaternion(webObject.quaternion);
+iframe.style.visibility =
+  normal.dot(camera.position.clone().sub(webObject.position).normalize()) < 0
+    ? "hidden"
+    : "visible";
 
 // Set webObject to Pannel Position
 webObject.position.copy(pannelPosition);

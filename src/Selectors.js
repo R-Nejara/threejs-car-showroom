f6a0a24b370edb5a0da3f93bedfba72186143c85
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-12 23:42:48 +0200

    chore: add selector for button-area in Selectors.js

--- src/Selectors.js
+++ src/Selectors.js
@@ -2,5 +2,6 @@
 const canvas = document.getElementById("main-canvas");
 const startButton = document.getElementById("start-button");
 const startText = document.getElementById("start-text");
+const startButtonArea = document.getElementById("button-area");
 
-export { canvas, startButton, startText };
+export { canvas, startButton, startText, startButtonArea };

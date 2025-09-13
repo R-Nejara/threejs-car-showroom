0370ecf8f84b3cb083958211ff91d2bc76e614b5
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-13 02:56:28 +0200

    chore: add selector for day night toggle in Selectors.js

--- src/Selectors.js
+++ src/Selectors.js
@@ -1,7 +1,19 @@
 // Canvas
 const canvas = document.getElementById("main-canvas");
+
+//Start Area
 const startButton = document.getElementById("start-button");
 const startText = document.getElementById("start-text");
 const startButtonArea = document.getElementById("button-area");
 
-export { canvas, startButton, startText, startButtonArea };
+//Overlay
+
+//Day Night Switch
+const dayNightSwitch = {
+  ball: document.querySelector(".ball"),
+  area: document.querySelector(".daynight-switch-area"),
+  moon: document.querySelector(".moon"),
+  sun: document.querySelector(".sun"),
+};
+
+export { canvas, startButton, startText, startButtonArea, dayNightSwitch };

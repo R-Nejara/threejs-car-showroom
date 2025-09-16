b0ac0dccbb84ac8efbaf055075d8002c5a43e0bf
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-16 22:30:29 +0200

    chore: add lantern position variables and light intensities in helpers.js
    adding array with x positions where lanterns are positioned. The Z
    Positions mark the left and right (*front* and *back*).

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,6 +6,12 @@ const carPosition = new THREE.Vector3(-2.5, 2, -1);
 const skyPosition = new THREE.Vector3(-5.5, 25, -3);
 const directionalLightPosition = new THREE.Vector3(25, 25, 25);
 
+// Lantern Positions
+const LANTERN_FRONT_Z = -9;
+const LANTERN_BACK_Z = 15;
+const LANTERN_Y = 5;
+const lanternPositionsX = [-13.5, -5.6, 1.1, 8.9];
+
 // Ground and Sky Y Constants
 const GROUND_LEVEL_SIDEWALK_Y = 0.6;
 const GROUND_LEVEL_STREET_Y = 0.4;
@@ -14,6 +20,8 @@ const SKY_LEVEL_Y = skyPosition.y;
 //Light Intensities
 const SPOTLIGHT_ON_INTENSITY = 1000;
 const SPOTLIGHTLIGHT_OFF_INTENSITY = 0;
+const LANTERNLIGHT_ON_INTENSITY = 3;
+const LANTERNLIGHT_OFF_INTENSITY = 0;
 
 // Initialize Sizes
 const sizes = {
@@ -44,4 +52,10 @@ export {
   mobileMedia,
   SPOTLIGHT_ON_INTENSITY,
   SPOTLIGHTLIGHT_OFF_INTENSITY,
+  LANTERNLIGHT_ON_INTENSITY,
+  LANTERNLIGHT_OFF_INTENSITY,
+  LANTERN_FRONT_Z,
+  LANTERN_BACK_Z,
+  LANTERN_Y,
+  lanternPositionsX,
 };

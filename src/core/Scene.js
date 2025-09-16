5e1d38ea5fc0df1e8c742abca0d4ea5e7d11615e
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-16 20:52:42 +0200

    chore: remove redundant import in Scene.js

--- src/core/Scene.js
+++ src/core/Scene.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { camera } from "./Camera.js";
 import { modelGroup } from "./Showcase.js";
-import { lights, spotLight } from "./Light.js";
+import { lights } from "./Light.js";
 import { nameText, titleText } from "./Text.js";
 // Initialize Scene
 const scene = new THREE.Scene();

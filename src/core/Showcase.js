e13a6ead553ad316c584d8181963ca8cfba41d90
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-17 19:49:22 +0200

    feat: add Car Base to carPartsNames and carArray
    still working on improvements that are more efficiant and reliable

--- src/core/Showcase.js
+++ src/core/Showcase.js
@@ -8,7 +8,7 @@ modelGroup.traverse((child) => {
 });
 
 // Get Headlights, Taillights, Mirrors and Car Base into car Array
-const carPartsNames = ["Headlights", "Taillights", "Mirror_Left", "Mirror_Right", "Mirror_Right_Holder", "Mirror_Left_Holder", "Tire_Front_Right", "Tire_Front_Left", "Tire_Back_Right", "Tire_Back_Left", "Front_Logo", "Tail_Logo"];
+const carPartsNames = ["Headlights", "Taillights", "Mirror_Left", "Mirror_Right", "Mirror_Right_Holder", "Mirror_Left_Holder", "Tire_Front_Right", "Tire_Front_Left", "Tire_Back_Right", "Tire_Back_Left", "Front_Logo", "Tail_Logo", "Car_Base"];
 const carArray = [];
 
 modelGroup.traverse((child) => {
@@ -20,28 +20,29 @@ modelGroup.traverse((child) => {
 // Organize Car Parts into an Usable Object
 const car = {
   mirror: {
-    leftBody: carArray[2],
-    leftHolder: carArray[3],
-    rightBody: carArray[4],
-    leftHolder: carArray[5],
+    leftBody: carArray[3],
+    leftHolder: carArray[4],
+    rightBody: carArray[5],
+    rightHolder: carArray[6],
   },
 
   lights: {
-    taillights: carArray[1],
-    headlights: carArray[0],
+    taillights: carArray[2],
+    headlights: carArray[1],
   },
 
   logo: {
-    front: carArray[10],
-    back: carArray[11],
+    front: carArray[11],
+    back: carArray[12],
   },
 
   tires: {
-    frontLeft: carArray[8],
-    frontRight: carArray[9],
-    backLeft: carArray[7],
-    backRight: carArray[6],
+    frontLeft: carArray[9],
+    frontRight: carArray[10],
+    backLeft: carArray[8],
+    backRight: carArray[7],
   },
+  allParts: carArray,
 }
 
 export { modelGroup, car };

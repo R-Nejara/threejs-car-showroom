0007aea71791cc875b9a721ab5061fec66cc8427
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-17 03:01:16 +0200

    feat: traverse through modelGroup to push car parts into carArray in Showcase.js

--- src/core/Showcase.js
+++ src/core/Showcase.js
@@ -7,4 +7,14 @@ modelGroup.traverse((child) => {
   child.receiveShadow = true;
 });
 
+// Get Headlights, Taillights, Mirrors and Car Base into car Array
+const carPartsNames = ["Headlights", "Taillights", "Mirror_Left", "Mirror_Right", "Mirror_Right_Holder", "Mirror_Left_Holder", "Tire_Front_Right", "Tire_Front_Left", "Tire_Back_Right", "Tire_Back_Left", "Front_Logo", "Tail_Logo"];
+const carArray = [];
+
+modelGroup.traverse((child) => {
+  if (carPartsNames.includes(child.name)) {
+    carArray.push(child);
+  }
+});
+
 export { modelGroup };

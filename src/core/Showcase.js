498cd1f9860d12d27cd42742ff183ebc718d8fa5
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-19 05:05:22 +0200

    refactor: organizing variables in Showcase.js
    places variables to the start of the document

--- src/core/Showcase.js
+++ src/core/Showcase.js
@@ -1,15 +1,17 @@
 import * as THREE from "three";
 import { getObjectGroup } from "../loaders/GlbLoader";
 
+// Initialize Map to Store Car Parts
+const carMap = new Map();
+// Get Headlights, Taillights, Mirrors and Car Base into car Array
+const carPartsNames = ["Headlights", "Taillights", "Mirror_Left", "Mirror_Right", "Mirror_Right_Holder", "Mirror_Left_Holder", "Tire_Front_Right", "Tire_Front_Left", "Tire_Back_Right", "Tire_Back_Left", "Front_Logo", "Tail_Logo", "Car_Base"];
+const carArray = [];
+
 const modelGroup = await getObjectGroup("/models/Golf5.glb");
 modelGroup.traverse((child) => {
   child.castShadow = true;
   child.receiveShadow = true;
 });
-
-// Get Headlights, Taillights, Mirrors and Car Base into car Array
-const carPartsNames = ["Headlights", "Taillights", "Mirror_Left", "Mirror_Right", "Mirror_Right_Holder", "Mirror_Left_Holder", "Tire_Front_Right", "Tire_Front_Left", "Tire_Back_Right", "Tire_Back_Left", "Front_Logo", "Tail_Logo", "Car_Base"];
-const carArray = [];
 
 modelGroup.traverse((child) => {
   if (carPartsNames.includes(child.name)) {

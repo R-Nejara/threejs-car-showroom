8d78dc4ae75dcd66113dd4d2ef6e38942ce1078e
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-21 03:07:00 +0200

    feat: add positions and intensities for car light in helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,6 +6,10 @@ const carPosition = new THREE.Vector3(-2.5, 2, -1);
 const skyPosition = new THREE.Vector3(-5.5, 25, -3);
 const directionalLightPosition = new THREE.Vector3(25, 25, 25);
 
+// Car Lights Positions
+const headlightsPosition = new THREE.Vector3().copy(carPosition).add(new THREE.Vector3(-5,1,0));
+const taillightsPosition = new THREE.Vector3().copy(carPosition).add(new THREE.Vector3(5,1,0));
+
 // Lantern Positions
 const LANTERN_FRONT_Z = -9;
 const LANTERN_BACK_Z = 15;
@@ -22,6 +26,8 @@ const SPOTLIGHT_ON_INTENSITY = 1000;
 const SPOTLIGHT_OFF_INTENSITY = 0;
 const LANTERNLIGHT_ON_INTENSITY = 3;
 const LANTERNLIGHT_OFF_INTENSITY = 0;
+const CARLIGHT_ON_INTENSITY = 100;
+const CARLIGHT_OFF_INTENSITY = 0;
 
 // Initialize Sizes
 const sizes = {
@@ -58,4 +64,8 @@ export {
   LANTERN_BACK_Z,
   LANTERN_Y,
   lanternPositionsX,
+  headlightsPosition,
+  taillightsPosition,
+  CARLIGHT_ON_INTENSITY,
+  CARLIGHT_OFF_INTENSITY,
 };

ade8ab6805c12a11eddf1c817ff6903bfc80ec7c
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-22 03:55:24 +0200

    perf: calling warmup() function in main.js
    This function pre renders a set of positions before entering the
    renderloop

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import "./style.css";
 import { runScene } from "./core/AnimationLoop";
+import { camera } from "./core/Camera";
+import { warmup } from "./core/Compile";
+import { controls } from "./controls/OrbitControls";
 
+warmup(camera, controls);
 runScene();

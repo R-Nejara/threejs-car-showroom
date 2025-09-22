f0219a77dc37d5f0ea649fa9069ae8f5d2bb2f77
Rayan Nejara <rayannejara22u@gmail.com>
2025-09-22 19:32:44 +0200

    feat: add Audio Files and implement play and mute logic in Audio.js

---
+++ src/core/Audio.js
@@ -0,0 +1,28 @@
+const switchSound = new Audio("audios/light-switch.mp3");
+const playSwitch = () => {
+  switchSound.play();
+}
+const muteButtonSound = new Audio("audios/button.mp3");
+const playButton = () => {
+  muteButtonSound.volume = 0.6
+  muteButtonSound.play();
+}
+
+const trafficSound = new Audio("audios/traffic.mp3")
+const playTraffic = () => {
+  trafficSound.volume = 0.6;
+  trafficSound.play();
+}
+const barButtonSound = new Audio("audios/bar_button.mp3")
+const playBarButton = () => {
+  barButtonSound.volume = 0.6;
+  barButtonSound.play()
+}
+
+const muteSounds = (isMuted) => {
+  trafficSound.muted = isMuted;
+  barButtonSound.muted = isMuted;
+  switchSound.muted = isMuted;
+}
+
+export { playSwitch, playTraffic, muteSounds, playButton, playBarButton }
